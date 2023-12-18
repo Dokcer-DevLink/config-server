009124ee2dcb4f79250b21635c0d906e9a4126fc
MoonStar <moonsung0331@gmail.com>
2023-12-18 12:10:56 +0900

    Init Project

---
+++ src/test/java/com/goorm/devlink/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goorm.devlink.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
